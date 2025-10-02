1068563f63b7e41c668aa206f0be4e21c6872376
Alexandr Kovyazin <sanokov97@mail.ru>
2025-10-02 09:08:32 +0300

    completed homework_9_1 by 29_09_25

---
+++ src/masks.py
@@ -0,0 +1,46 @@
+def get_mask_card_number(user_input_card_number: str) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    counter: int = 0
+    masked_card_number: str = ""
+    len_card = len(user_input_card_number)
+    start_num = 6  # количество цифр, отображаемых в начале
+    end_num = 4  # количество цифр, отображаемых в конце
+
+    for num in user_input_card_number:
+
+        if counter % 4 == 0:  # ставим разделитель через 4 знака
+            masked_card_number += " "
+
+        counter += 1
+
+        if counter < start_num + 1:  # показываем первые цифры
+            masked_card_number += num
+            continue
+
+        if counter > (len_card - end_num):  # показываем пеоследние цифры
+            masked_card_number += num
+            continue
+
+        masked_card_number += "*"  # остальные не показываем
+
+    return masked_card_number
+
+
+def get_mask_account(user_input_account: str) -> str:
+    """Функция маскировки номера банковского счетa"""
+
+    counter: int = 0
+    masked_account: str = "**"
+    len_account = len(user_input_account)
+    end_num = 4  # количество цифр, отображаемых в конце
+
+    for num in user_input_account:
+
+        counter += 1
+
+        if counter > (len_account - end_num):  # показываем пеоследние цифры
+            masked_account += num
+            continue
+
+    return masked_account
