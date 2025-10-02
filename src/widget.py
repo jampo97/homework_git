717b102074b7296d6c0523c0de4858bfad80154a
Alexandr Kovyazin <sanokov97@mail.ru>
2025-10-02 10:58:50 +0300

    add func date formatin in widget.py

--- src/widget.py
+++ src/widget.py
@@ -6,8 +6,8 @@ def mask_account_card(user_input: str) -> str:
     """функция маскировки введенных данных (счета или номера карты)"""
 
     masked_account_card: str = ""
-    card_or_account: str = account_or_card(user_input)[0]   # результат проверки - карта/счет
-    count_numbers = int(account_or_card(user_input)[1])     # количество цифр в вводе пользователя
+    card_or_account: str = account_or_card(user_input)[0]  # результат проверки - карта/счет
+    count_numbers = int(account_or_card(user_input)[1])  # количество цифр в вводе пользователя
 
     if card_or_account == "card":
         user_input_card_number = user_input[-count_numbers:]
@@ -18,8 +18,6 @@ def mask_account_card(user_input: str) -> str:
         user_input_account_number = user_input[-count_numbers:]
         masked_number = get_mask_account(user_input_account_number)
         masked_account_card = user_input[:-count_numbers] + masked_number
-
-
 
     return masked_account_card
 
@@ -47,9 +45,22 @@ def account_or_card(user_input: str) -> list[str]:
     return [card_or_account, counter]
 
 
+def get_date(input_date: str) -> str:
+    """форматирование данных о дате в формат ДД.ММ.ГГГГ"""
+
+    date_format: str = ""
+    year: str = input_date[0:4]
+    month: str = input_date[5:7]
+    day: str = input_date[8:10]
+
+    date_format = f"{day}.{month}.{year}"
+
+    return date_format
+
 
 inputs: list[str] = ["Maestro 1596837868705199", "Счет 64686473678894779589", "MasterCard 7158300734726758",
-          "Счет 35383033474447895560", "Visa Classic 6831982476737658", "Visa Platinum 8990922113665229",
-          "Visa Gold 5999414228426353", "Счет 73654108430135874305"]
+                     "Счет 35383033474447895560", "Visa Classic 6831982476737658", "Visa Platinum 8990922113665229",
+                     "Visa Gold 5999414228426353", "Счет 73654108430135874305"]
 for item in inputs:
-    print (mask_account_card(item))
+    print(mask_account_card(item))
+print(get_date("2024-03-11T02:26:18.671407"))
