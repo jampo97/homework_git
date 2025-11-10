02ec7b3934dea2c2691bb2f6b889d1711dda54d2
Alexandr <jampo97@jmail.com>
2025-11-10 14:14:48 +0300

    Create sorting by date

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,15 @@ def filter_by_state(all_dics, state_filter="EXECUTED") -> list:
 
     return filtred_all_dics
 
+
+def sort_by_date(all_dics, sorting_rule) -> list:
+    """Функция сортировки по дате. По умолчанию - убывание"""
+
+    sorted_list: list = []
+
+    if sorting_rule == "reverse":
+        sorted_list = sorted(all_dics, key=lambda dic: dic["date"], reverse=False)
+    else:
+        sorted_list = sorted(all_dics, key=lambda dic: dic["date"], reverse=True)
+
+    return sorted_list
