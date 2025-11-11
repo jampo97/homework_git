4b2d166ea85d0fba6ea2f9bb6690901bfbf17d6b
Alexandr <jampo97@jmail.com>
2025-11-11 10:19:11 +0300

    linter correction

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
-def filter_by_state(all_dics, state_filter="EXECUTED") -> list:
-    """" Функция фильтрации по состоянию(state)"""
+def filter_by_state(all_dics: list, state_filter: str = "EXECUTED") -> list:
+    """ Функция фильтрации по состоянию(state)"""
 
     filtred_all_dics: list = []
 
     for dic in all_dics:
-        if dic['state'] == state_filter:
+        if dic["state"] == state_filter:
             filtred_all_dics.append(dic)
 
     return filtred_all_dics
 
 
-def sort_by_date(all_dics, sorting_rule) -> list:
+def sort_by_date(all_dics: list, sorting_rule: str) -> list:
     """Функция сортировки по дате. По умолчанию - убывание"""
 
     sorted_list: list = []
