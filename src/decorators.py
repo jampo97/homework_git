b2b8d1638517c6eee878f3ce1971fabaece45f91
Alexandr <jampo97@jmail.com>
2025-12-03 12:22:38 +0300

    create file decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,66 @@
+from functools import wraps
+from time import time
+
+
+def log(filename=None):
+    def my_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            #начало работы декоратора
+            start_time = time()
+
+            # блок отвечающий за ошибки до вызова функции
+            if len(args)!=2:
+                print("Введено неверное количество аргументов")
+                return None
+
+            # блок отвечающий за ошибки при вызове функции
+            try:
+                result = func(*args)
+            except TypeError:
+                print("Введите 2 числа")
+                return None
+            except ZeroDivisionError:
+                print("Ошибка: деление на ноль недопустимо.")
+                return None
+
+            # приводим значения для логов
+            log_1 = (f"Имя функции: {my_function},\n"
+                     f"Аргументы x и у = {args[0]} и {args[1]},\n"
+                     f"Время начала функции: {start_time},"
+                     f"\n___________\n"
+                     f"Результат выполнения функции:\n")
+
+            log_2 = (f"\n__________\n"
+                     f"Время конца функции: {time()},\n"
+                     f"Затраченное время: {time() - start_time}")
+
+            # блок для вывода текста в консоль
+            if filename is None or filename == "":
+
+                print (log_1)
+                print (result)
+                print (log_2)
+
+            # блок для вывода текста в файл
+            else:
+                with open(filename, "w", encoding="utf--8") as file:
+                    file.write(log_1)
+                    file.write(str(result))
+                    file.write(log_2)
+
+            return result
+
+        return wrapper
+
+    return my_decorator
+
+
+@log()
+def my_function(x, y):
+    """Функция деления"""
+    return x / y
+
+
+my_function(2,2)
